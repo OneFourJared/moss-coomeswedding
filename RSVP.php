1c149f63e805b7b603138dbc324e7e24fd9700e8
OneFourJared <61378686+OneFourJared@users.noreply.github.com>
2021-05-26 21:49:13 +0100

    Added navbar

--- RSVP.php
+++ RSVP.php
@@ -6,6 +6,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 </head>
 <body>
+<?php include('includes/navigation.php'); ?>
     <div class="container">
         <div class="row text-center">
             <div class="col">
