f49beeec9243e7d5524cffa4b4977637660d9774
OneFourJared <61378686+OneFourJared@users.noreply.github.com>
2021-06-05 01:21:04 +0100

    Updated styling

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,10 +1,10 @@
 <div class="container-fluid px-0">
-            <nav class="navbar navbar-expand-md navbar-light bg-cream">
-                <a class="navbar-brand mx-3 font-handwrite font-large" href="/">Wedding</a>
-                <button class="navbar-toggler mr-3" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <nav class="navbar navbar-expand-md navbar-light bg-cream px-3">
+                <a class="navbar-brand font-handwrite font-large" href="/">Wedding</a>
+                <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse mx-4" id="navbarNav">
+                </button>˚
+                <div class="collapse navbar-collapse mx-1" id="navbarNav">
                     <ul class="navbar-nav w-100 justify-content-end">
                         <li class="nav-item">
                             <a class=" nav-link" href="/">Home</a>
