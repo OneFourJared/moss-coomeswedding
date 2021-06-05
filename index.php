8d13b61efb8533399248d21819ebe1fc35ede4d9
OneFourJared <61378686+OneFourJared@users.noreply.github.com>
2021-06-05 01:00:30 +0100

    Updated styling

--- index.php
+++ index.php
@@ -20,15 +20,15 @@
 </div>
 <div class="container-fluid px-0">
     <div class="row mt-3">
-        <div class="col-md-4 my-auto">
+        <div class="col-md-5 my-auto">
             <div class="text-center font-main">
                 <h1>Kate</h1>
                 <h4 class="font-handwrite">and</h4>
                 <h1>Jared</h1>
             </div>
         </div>
-        <div class="col-md-8 my-auto">
-            <div class="text-center p-3">
+        <div class="col-md-7 my-auto">
+            <div class="text-center p-5">
                <div class="home-img">
                </div>
             </div>
