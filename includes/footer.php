1aba7f38c0aa5f4082d2e84efe4f8baf356bc3c8
OneFourJared <61378686+OneFourJared@users.noreply.github.com>
2021-06-10 22:32:00 +0100

    Home page

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,8 @@
 <div class="container-fluid px-0 bg-cream">
    <div class="row mt-2">
         <div class="col-md-12 text-center">
-            <div class="text-white py-2">
-                <h4>Kate Howe & Jared Moss-Coomes | 2022</h4>
+            <div class="py-2">
+                <h4 class="font-handwrite">Kate Howe & Jared Moss-Coomes | 2022</h4>
             </div>
         </div>
    </div>
--- index.php
+++ index.php
@@ -12,58 +12,63 @@
 
 <div class="container-fluid px-0">
     <div class="row">
-        <div class="col-12">
+        <div class="col-12 my-auto">
             <div class="home-back">
+                <div class="text-center font-main">
+                    <h1>Kate</h1>
+                    <h4 class="font-handwrite">and</h4>
+                    <h1>Jared</h1>
+                </div>
             </div>
         </div>
     </div>
 </div>
-<div class="container-fluid px-0">
-    <div class="row mt-3">
-        <div class="col-md-5 my-auto">
-            <div class="text-center font-main">
-                <h1>Kate</h1>
-                <h4 class="font-handwrite">and</h4>
-                <h1>Jared</h1>
-            </div>
-        </div>
-        <div class="col-md-7 my-auto">
-            <div class="text-center p-5">
-               <div class="home-img">
-               </div>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="container">
-    <div class="row">
-        <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
-            <img src="https://i.ibb.co/M8zGsQj/landscape1.jpg"
-                    class="w-100 shadow-1-strong rounded mb-4"
-                    alt=""/>
-            <img src="https://i.ibb.co/555Gc4D/portrait1.jpg"
-                    class="w-100 shadow-1-strong rounded mb-4"
-                    alt=""/>
-        </div>
-        <div class="col-lg-4 mb-4 mb-lg-0">
-            <img src="https://i.ibb.co/rxP2QTz/portrait2.jpg"
-                 class="w-100 shadow-1-strong rounded mb-4"
-                 alt=""/>
-
-            <img src="https://i.ibb.co/pQmvvcd/landscape4.jpg"
-                    class="w-100 shadow-1-strong rounded mb-4"
-                    alt=""/>
-        </div>
-        <div class="col-lg-4 mb-4 mb-lg-0">
-            <img src="https://i.ibb.co/hX75ybn/landscape3.jpg"
-                    class="w-100 shadow-1-strong rounded mb-4"
-                    alt=""/>
-            <img src="https://i.ibb.co/p0Lk27D/landscape2.jpg"
-                 class="w-100 shadow-1-strong rounded mb-4"
-                 alt=""/>
-        </div>
-    </div>
-</div>
+<!--<div class="container-fluid px-0">-->
+<!--    <div class="row mt-3">-->
+<!--        <div class="col-md-5 my-auto">-->
+<!--            <div class="text-center font-main">-->
+<!--                <h1>Kate</h1>-->
+<!--                <h4 class="font-handwrite">and</h4>-->
+<!--                <h1>Jared</h1>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--        <div class="col-md-7 my-auto">-->
+<!--            <div class="text-center p-5">-->
+<!--               <div class="home-img">-->
+<!--               </div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--</div>-->
+<!--<div class="container">-->
+<!--    <div class="row">-->
+<!--        <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">-->
+<!--            <img src="https://i.ibb.co/M8zGsQj/landscape1.jpg"-->
+<!--                    class="w-100 shadow-1-strong rounded mb-4"-->
+<!--                    alt=""/>-->
+<!--            <img src="https://i.ibb.co/555Gc4D/portrait1.jpg"-->
+<!--                    class="w-100 shadow-1-strong rounded mb-4"-->
+<!--                    alt=""/>-->
+<!--        </div>-->
+<!--        <div class="col-lg-4 mb-4 mb-lg-0">-->
+<!--            <img src="https://i.ibb.co/rxP2QTz/portrait2.jpg"-->
+<!--                 class="w-100 shadow-1-strong rounded mb-4"-->
+<!--                 alt=""/>-->
+<!---->
+<!--            <img src="https://i.ibb.co/pQmvvcd/landscape4.jpg"-->
+<!--                    class="w-100 shadow-1-strong rounded mb-4"-->
+<!--                    alt=""/>-->
+<!--        </div>-->
+<!--        <div class="col-lg-4 mb-4 mb-lg-0">-->
+<!--            <img src="https://i.ibb.co/hX75ybn/landscape3.jpg"-->
+<!--                    class="w-100 shadow-1-strong rounded mb-4"-->
+<!--                    alt=""/>-->
+<!--            <img src="https://i.ibb.co/p0Lk27D/landscape2.jpg"-->
+<!--                 class="w-100 shadow-1-strong rounded mb-4"-->
+<!--                 alt=""/>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--</div>-->
 <?php include('includes/footer.php'); ?>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
