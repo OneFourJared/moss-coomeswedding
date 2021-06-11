c48c7561cfd9b2b7749c5a777102f8a785b55449
OneFourJared <61378686+OneFourJared@users.noreply.github.com>
2021-06-11 21:30:06 +0100

    Home page

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
    <div class="row">
         <div class="col-md-12 text-center vertical-center">
             <div class="py-2">
-                <h4 class="font-handwrite">Kate Howe & Jared Moss-Coomes | 2022</h4>
+                <h4 class="font-handwrite mb-0 text-white">Kate Howe & Jared Moss-Coomes | 2022</h4>
             </div>
         </div>
    </div>
