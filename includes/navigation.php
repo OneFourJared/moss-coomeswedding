5f76fe09e83fdd570864791d3b7b2c0f87fe1900
OneFourJared <61378686+OneFourJared@users.noreply.github.com>
2021-06-11 21:22:42 +0100

    Home page

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,23 +1,23 @@
 <div class="container-fluid px-0">
-            <nav class="navbar navbar-expand-md navbar-dark bg-transparent px-5">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav w-100 justify-content-center text-white font-handwrite">
-                        <li class="nav-item">
-                            <a class=" nav-link" href="/">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Agenda</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/RSVP">RSVP</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Contact</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+        <nav class="navbar navbar-expand-md navbar-dark bg-nav-gradient px-5">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav w-100 justify-content-center text-white font-handwrite">
+                    <li class="nav-item">
+                        <a class=" nav-link" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Agenda</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/RSVP">RSVP</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
 </div>
