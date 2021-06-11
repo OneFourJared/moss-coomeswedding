a18fcb3bdb8d30f1729e49398d6f042eaead85c7
OneFourJared <61378686+OneFourJared@users.noreply.github.com>
2021-06-11 21:06:09 +0100

    Home page

--- includes/fonts.php
+++ includes/fonts.php
@@ -1,4 +1,5 @@
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Mate+SC&family=Parisienne&display=swap');
     @import url('https://fonts.googleapis.com/css2?family=League+Script&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Rajdhani:wght@300;400&display=swap');
 </style>
