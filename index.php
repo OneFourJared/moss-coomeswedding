ea2365c8509e885c767d5fd427fb4c14980f389a
OneFourJared <61378686+OneFourJared@users.noreply.github.com>
2021-06-26 21:59:25 +0100

    Test

--- index.php
+++ index.php
@@ -8,9 +8,29 @@
     <?php include('includes/fonts.php'); ?>
 </head>
 <body>
-<?php include('includes/navigation.php'); ?>
 
 <div class="container-fluid px-0">
+    <div class="row">
+        <nav class="navbar navbar-expand-md navbar-dark bg-nav-gradient px-5">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav w-100 justify-content-center text-white font-handwrite">
+                    <li class="nav-item">
+                        <a class=" nav-link" href="/">Home</a>
+                    </li>
+                    <!--                    <li class="nav-item">-->
+                    <!--                        <a class="nav-link" href="#">Agenda</a>-->
+                    <!--                    </li>-->
+                    <li class="nav-item">
+                        <a class="nav-link" href="/RSVP">RSVP</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+
     <div class="row my-auto">
         <div class="col-12">
             <div class="home-back vertical-center">
