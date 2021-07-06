f688188c4800176c3052377966e0998be9e53a43
OneFourJared <61378686+OneFourJared@users.noreply.github.com>
2021-07-06 13:19:50 +0100

    Test

--- RSVP.php
+++ RSVP.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1" >
     <title>RSVP</title>
     <link rel="stylesheet" type="text/css" href="main.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
