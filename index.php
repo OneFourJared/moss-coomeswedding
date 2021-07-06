1cb2fc7495fd508b5b8618c4b044921e2f3abf21
OneFourJared <61378686+OneFourJared@users.noreply.github.com>
2021-07-06 21:04:14 +0100

    Test

--- index.php
+++ index.php
@@ -105,8 +105,19 @@
     }, 1000);
 </script>
 <script>
-    $(window).load(function() {
-        $('#spinner').hide();
+    (window).on('load', function(){
+        setTimeout(removeLoader, 2000); //wait for page load PLUS two seconds.
+    });
+    function removeLoader(){
+        ( "#spinnrt" ).fadeOut(500, function() {
+            // fadeOut complete. Remove the loading div
+            $( "#spinner" ).remove(); //makes page more lightweight
+        });
+    }
+</script>
+<script>
+    (window).load(function() {
+        ('#spinner').hide();
     });
 </script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
